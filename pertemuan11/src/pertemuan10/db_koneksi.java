ad8b107afbfabc46af2b643141f6c228b5113f14
Decay <110650049+dewacahaya@users.noreply.github.com>
2025-06-14 10:33:44 +0800

    pertemuan 11-12

---
+++ pertemuan11/src/pertemuan10/db_koneksi.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan10;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Pongo
+ */
+public class db_koneksi {
+    static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static String DB_URL = "jdbc:mysql://localhost/buku_alamat";
+    static String DB_USER = "root";
+    static String DB_PASS = "";
+    static Connection koneksi;
+    
+    static Connection koneksi(){
+        try {
+            Class.forName(JDBC_DRIVER);
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Terjadi masalah koneksi ke DB");
+        }
+        return null;
+    }
+}
